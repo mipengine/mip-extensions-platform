e82348602a4b8a7dda6b7f9cb799871493409e7a
mip-platform <kekee000@sina.com>
2017-06-26 16:23:18 +0800

    update extensions

---
+++ mip-233-djs/README.md
@@ -0,0 +1,17 @@
+# mip-233-djs
+
+倒计时功能
+
+标题|内容
+----|----
+类型|业务,广告
+支持布局|N/S
+所需脚本|https://mipcache.bdstatic.com/static/v1/mip-233-djs/mip-233-djs.js
+
+## 示例
+
+``` html
+<mip-233-djs data-start="2017-6-25">
+    <div class="countdown">距<span color="red">四川</span>考试还有<span class="time">95</span><span>天</span></div>
+</mip-233-djs>
+```
---
+++ mip-233-djs/mip-233-djs.js
@@ -0,0 +1,37 @@
+/**
+ * @file 倒计时功能
+ * @author 233 程序部
+ */
+define(function (require) {
+    var $ = require('zepto');
+    var customElem = require('customElement').create();
+    // build 方法，元素插入到文档时执行，仅会执行一次
+    customElem.prototype.build = function () {
+        var element = this.element;
+        var date = new Date();
+        var start = $(element).attr('data-start');
+        var now = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+        if (!start) {
+            $(element).hide();
+            return;
+        }
+        function dateDiff(sDate1, sDate2) {
+            var aDate;
+            var oDate1;
+            var oDate2;
+            var iDays;
+            aDate = sDate1.split('-');
+            oDate1 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0]);
+            aDate = sDate2.split('-');
+            oDate2 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0]);
+            iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24, 10);
+            if (oDate2 > oDate1) {
+                iDays = 0;
+            }
+            return iDays;
+        }
+        var diff = dateDiff(start, now);
+        $(element).find('.time').html(diff);
+    };
+    return customElem;
+});
