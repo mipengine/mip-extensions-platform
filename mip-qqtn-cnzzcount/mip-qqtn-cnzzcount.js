271e758f92cdd656740672a6d5f4b1385c325a88
mip-platform <kekee000@sina.com>
2017-07-05 17:08:17 +0800

    update extensions

--- mip-qqtn-cnzzcount/mip-qqtn-cnzzcount.js
+++ mip-qqtn-cnzzcount/mip-qqtn-cnzzcount.js
@@ -9,23 +9,17 @@
 define(function (require) {
     var $ = require('zepto');
     var customElement = require('customElement').create();
-    var pageAttr = {
-        rootId: $('.f-information').attr('data-rootid'),
-        healthRootId: $('.f-health-rootid').html().split(',')
-    };
     customElement.prototype.build = function () {
-        if ($.inArray(pageAttr.rootId, pageAttr.healthRootId) === -1) {
-            $('body').append('<span id=\'cnzz_stat_icon_5932461\'></span>');
-            var cnzzCount = document.createElement('script');
-            cnzzCount.src = 'https://s12.cnzz.com/z_stat.php?id=5932461&web_id=5932461';
-            $('body').append(cnzzCount);
-        }
-        else {
-            $('body').append('<span id=\'cnzz_stat_icon_1257361975\'></span>');
-            var cnzzCount = document.createElement('script');
-            cnzzCount.src = 'https://s12.cnzz.com/z_stat.php?id=1257361975&web_id=1257361975';
-            $('body').append(cnzzCount);
-        }
-    };
+            var dom = document.getElementById('qqtncnzz');
+            var sc = document.createElement('script');
+            sc.setAttribute('type', 'text/javascript');
+            sc.setAttribute('src', 'https://s12.cnzz.com/z_stat.php?id=5932461&web_id=5932461');
+            dom.appendChild(sc);
+            var domh = document.getElementById('healthcnzz');
+            var sch = document.createElement('script');
+            sch.setAttribute('type', 'text/javascript');
+            sch.setAttribute('src', 'https://s12.cnzz.com/z_stat.php?id=1257361975&web_id=1257361975');
+            domh.appendChild(sch);
+        };
     return customElement;
 });
