28568d3451abdc9ccb5f8c7e9a62896ddd8ea4e3
mip-platform <kekee000@sina.com>
2017-07-06 20:27:17 +0800

    update extensions

--- mip-cr173-cnzz/mip-cr173-cnzz.js
+++ mip-cr173-cnzz/mip-cr173-cnzz.js
@@ -1,32 +1,20 @@
 /**
-* @file CNZZ统计插件
+* @file CNZZ统计
 * @exports modulename
 * @author chenrui09@baidu.com
-* @version 1.0
+* @version 1.0.3
 * @copyright 2016 Baidu.com, Inc. All Rights Reserved
 */
 
 define(function (require) {
     var $ = require('zepto');
     var customElement = require('customElement').create();
-    var pageAttr = {
-        rootId: $('.f-information').attr('data-rootid'),
-        healthRootId: $('.f-health-rootid').html().split(',')
-    };
     customElement.prototype.build = function () {
-        if ($.inArray(pageAttr.rootId, pageAttr.healthRootId) === -1) {
-            $('body').append('<span id=\'cnzz_stat_icon_3608757\'></span>');
-            var cnzzCount = document.createElement('script');
-            cnzzCount.src = 'https://s12.cnzz.com/z_stat.php?id=3608757&web_id=3608757';
-            $('body').append(cnzzCount);
-        }
-        else {
-            $('body').append('<span id=\'cnzz_stat_icon_3608757\'></span>');
-            var cnzzCount = document.createElement('script');
-            cnzzCount.src = 'https://s12.cnzz.com/z_stat.php?id=3608757&web_id=3608757';
-            $('body').append(cnzzCount);
-        }
-    };
+            var dom = document.getElementById('mip-cr173-cnzz');
+            var sc = document.createElement('script');
+            sc.setAttribute('type', 'text/javascript');
+            sc.setAttribute('src', 'http://w.cnzz.com/c.php?id=3608757');
+            dom.appendChild(sc);
+        };
     return customElement;
 });
-
