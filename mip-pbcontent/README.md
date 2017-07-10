8a7fc6ede6fda34276fc1c886131444431409ddc
mip-platform <kekee000@sina.com>
2017-07-10 14:13:05 +0800

    update extensions

---
+++ mip-pbcontent/README.md
@@ -0,0 +1,32 @@
+# mip-pbcontent
+
+mip-pbcontent 组件说明
+
+标题|内容
+----|----
+类型|通用
+支持布局|responsive,fixed-height,fill,container,fixed
+所需脚本|https://mipcache.bdstatic.com/static/v1/mip-pbcontent/mip-pbcontent.js
+
+## 示例
+
+### 基本用法
+```html
+<mip-pbcontent type="soft" pid="477942" at="myxdownsy" titl="youxi">
+    自定义内容，可以嵌套其他组件
+</mip-pbcontent>
+```
+
+## 属性
+
+### {属性名}
+
+说明：{说明}
+必选项：{是|否}
+类型：{类型}
+取值范围：{取值范围}
+单位：{单位}
+默认值：{默认值}
+
+## 注意事项
+
---
+++ mip-pbcontent/mip-pbcontent.js
@@ -0,0 +1,26 @@
+/**
+ * @file mip-pbcontent 组件
+ * @author
+ */
+
+define(function (require) {
+    var customElement = require('customElement').create();
+    customElement.prototype.build = function () {
+        var $ = require('jquery');
+        var element = this.element;
+        var pt = element.getAttribute('type');
+        var id = element.getAttribute('pid');
+        var at = element.getAttribute('at');
+        var title = element.getAttribute('titl');
+        var href = location.href;
+        if (pt && id && at) {
+            window.pb = function (data) {
+                if (data) {
+                    window.location = '/404?pb=' + at + '&href=' + href;
+                }
+            };
+            $.getScript('http://pb.sys.pp8.com/getpb.do?pt=' + pt + '&id=' + id + '&at=' + at + '&callback=pb&title=' + title);
+        }
+    };
+    return customElement;
+});
