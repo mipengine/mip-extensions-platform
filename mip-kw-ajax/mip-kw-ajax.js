5c60a88d78366b41c081597365f2f0ce9b1daa92
mip-platform <kekee000@sina.com>
2017-07-31 18:29:10 +0800

    update extensions

--- mip-kw-ajax/mip-kw-ajax.js
+++ mip-kw-ajax/mip-kw-ajax.js
@@ -1,65 +1,32 @@
 /**
- * @file mip-ajax-data 组件
- * @author Jobs Fan
+ * @file mip-kw-ajax 组件
+ * @author Wang Tao
+ * @version 1.0.0
  */
 
 define(function (require) {
 
     var customElement = require('customElement').create();
-    var $ = require('zepto');
-
-    /** [bindEven 绑定事件]
-     *
-     * @param {Object} element [mip-ajax-data元素]
-     * @param {Object} params [来自mip-ajax-data的属性]
-     * @param {Booleans} once [是否只执行一次]
-     */
-    // function bindEven(element, params, once) {
-        // window.onload(function () {
-        //     var id = params.id;
-        //
-        //     fetch(params.updateVideoCount, {
-        //         method: 'get',
-        //         body: {id: id}
-        //     }).then(function (res) { }).then(function (text) { });
-        //
-        //     var url = params.url;
-        //     fetch(url, {
-        //         method: 'get',
-        //         body: {id: id}
-        //     }).then(function (res) {
-        //         if (res.code === 0) {
-        //             $('.' + params.containerclass).html(res.data.CLICKNUM + ' 阅读量');
-        //         }
-        //     }).then(function (text) { });
-        // });
-    // }
 
     /** [构造元素，只会运行一次]
      *
      */
     customElement.prototype.build = function () {
-        var self = this;
-        var element = this.element;
-        var params = JSON.parse($(element).attr('mip-ajax-params').replace(/'/g, '"'));
-        // bindEven(element, params, typeof ($(element).attr('mip-ajax-params')) === 'undefined');
+        var params = document.getElementById('mipKwAjax');
 
-        var id = params.id;
-
-        fetch('http://www.365tang.cn/mip/updateVideoCount', {
-            method: 'get',
-            body: {id: id}
+        params = params.attr('mip-ajax-params');
+        fetch('https://www.365tang.cn/mip/updateVideoCount?id= ' + params, {
+            method: 'get'
         }).then(function (res) { }).then(function (text) { });
-
-        fetch('http://www.365tang.cn/mip/getVideoCount', {
-            method: 'get',
-            body: {id: id}
+        fetch('https://www.365tang.cn/mip/getVideoCount?id= ' + params, {
+            method: 'get'
+        }).then(function (res) {
+            return res.json();
         }).then(function (res) {
             if (res.code === 0) {
-                $('.praise').html(res.data.CLICKNUM + ' 阅读量');
+                document.getElementById('praise').html(res.data.CLICKNUM + ' 阅读量');
             }
-        }).then(function (text) { });
+        });
     };
-
     return customElement;
 });
