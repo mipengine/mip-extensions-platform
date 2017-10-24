0ac2e69da61c138ae976548446e3c14c1760ca25
mip-platform <mip-platform@baidu.com>
2017-10-24 17:04:50 +0800

    update extensions

--- mip-xzw-rem/mip-xzw-rem.js
+++ mip-xzw-rem/mip-xzw-rem.js
@@ -2,14 +2,14 @@
 * 星座屋mip改造
 * @file 星座屋rem组件
 * @author mipxzw@163.com
-* @version 1.0.0
+* @version 1.0.1
 */
 define(function (require) {
     var $ = require('zepto');
     var customElem = require('customElement').create();
     customElem.prototype.build = function () {
         var html = $('html');
-        var hW = html.outerWidth() > 640 ? 640 : html.outerWidth();
+        var hW = $(window).width() > 640 ? 640 : $(window).width();
         var rem = hW / 10;
         html.css('fontSize', rem);
     };
