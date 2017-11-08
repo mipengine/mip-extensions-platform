a53f89f8de5331bea6a894dfa0837539839a16d9
mip-platform <mip-platform@baidu.com>
2017-11-08 16:57:08 +0800

    update extensions

--- mip-169kangad/README.md
+++ mip-169kangad/README.md
@@ -14,7 +14,7 @@ mip-169kangad 用来添加搜狗广告。
 MIP提供搜狗广告的扩展组件，代码示例：
 
 ```html
-	<mip-169kangad ad_id="912233" ad_width="20" ad_height="2"></mip-169kangad>     
+	<mip-169kangad ad_id="912233" ad_width="20" ad_height="2" ad_index="1"></mip-169kangad>     
 ```
 
 ## 属性
@@ -24,6 +24,13 @@ MIP提供搜狗广告的扩展组件，代码示例：
 说明：广告ID
 必选项：是
 类型：字符串
+
+### ad_index
+
+说明：广告位置
+必选项：否
+类型：整数
+默认：1
 
 ### ad_width
 
--- mip-169kangad/mip-169kangad.js
+++ mip-169kangad/mip-169kangad.js
@@ -11,15 +11,16 @@ define(function (require) {
         var adId = ele.getAttribute('ad_id');
         var width = ele.getAttribute('ad_width') ? ele.getAttribute('ad_width') : 20;
         var height = ele.getAttribute('ad_height') ? ele.getAttribute('ad_height') : 2;
+        var index = ele.getAttribute('ad_index') ? ele.getAttribute('ad_index') : 1;
         var div = document.createElement('div');
         var divattr = document.createAttribute('id');
-        divattr.value = 'sogou_wap_' + adId;
+        divattr.value = 'sogou_wap_' + adId + index;
         div.setAttributeNode(divattr);
         var scriptTag1 = document.createElement('script');
         scriptTag1.src = 'https://theta.sogoucdn.com/wap/js/aw.js',
         scriptTag1.setAttribute('async', 'true');
         var scriptTag2 = document.createElement('script');
-        scriptTag2.innerHTML = 'var sogou_div = document.getElementById("sogou_wap_' + adId + '");'
+        scriptTag2.innerHTML = 'var sogou_div = document.getElementById("sogou_wap_' + adId + index + '");'
 		+ 'window.sogou_un = window.sogou_un || [];window.sogou_un.push({id:"'
         + '' + adId + '",ele:sogou_div,w:' + '' + width + ',h:' + '' + height + '});';
         ele.appendChild(div);
