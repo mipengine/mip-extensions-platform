9cd13fe3986d2ebb9030b9321ed023d142200725
mip-platform <mip-platform@baidu.com>
2017-12-14 14:33:29 +0800

    update extensions

--- mip-jia-distance/README.md
+++ mip-jia-distance/README.md
@@ -17,9 +17,12 @@ mip-jia-distance 组件说明
     {
         "ak": "243f68afdbfe0aa70ca23117c4606b94",
         "address": "上海市普陀区真南路1228号齐家建材家居馆2楼E-09",
+        "type": "map",
+        "zoom": 18,
+        "link": "//m.jia.com/wangpu/shop/map/5188",
         "unit": "千米",
-        "beforeText": "距您现在位置约",
-        "afterText": "km"
+        "beforeText": "距您约",
+        "afterText": "千米"
     }
     </script>
 </mip-jia-distance>
@@ -61,6 +64,28 @@ mip-jia-distance 组件说明
 类型：string
 默认值：""
 
+### type
+
+说明：根据type类型判断组件样式与功能（地图定位与显示用户位置距离）
+必选项：是
+类型：string
+取值范围：map, distance
+默认值：distance
+
+### zoom
+
+说明：百度地图比例尺
+必选项：否
+类型：number
+取值范围：1-18
+默认值：18
+
+### link
+
+说明：跳转链接
+必选项：否
+类型：string
+默认值：""
+
 
 ## 注意事项
-
--- mip-jia-distance/mip-jia-distance.js
+++ mip-jia-distance/mip-jia-distance.js
@@ -6,9 +6,19 @@
 define(function (require) {
 
     var customElement = require('customElement').create();
-    var MAPURL = 'https://api.map.baidu.com/api?';
+    var MAPURL = 'https://api.map.baidu.com/getscript?';
     var TYPE = 'script[type="application/json"]';
+    var baiduMapApiLoaded = false;
 
+    function jsonParse(json) {
+        try {
+            return JSON.parse(json);
+        }
+        catch (e) {
+            console.error(e);
+            return false;
+        }
+    }
 
     /**
      * 地图类
@@ -22,17 +32,6 @@ define(function (require) {
         this.ele = element;
     }
 
-
-    /**
-     * 展现地图逻辑入口
-     *
-     */
-    BaiduMap.prototype.show = function () {
-        this.init();
-        this.append();
-    };
-
-
     /**
      * 初始化地图请求 URL
      *
@@ -43,8 +42,7 @@ define(function (require) {
         var pObj = {
             v: cf.version || '2.0',
             ak: cf.ak || '',
-            t: new Date().getTime(),
-            callback: this.getCb()
+            t: new Date().getTime()
         };
         for (var key in pObj) {
             if (pObj.hasOwnProperty(key)) {
@@ -54,59 +52,32 @@ define(function (require) {
         this.mapUrl = MAPURL + pArray.join('&');
     };
 
-
-    /**
-     * 绑定全局 callback 函数，并返回回调名称
-     *
-     * @return {string} 回调名称
-     */
-    BaiduMap.prototype.getCb = function () {
-        window.mapCallback = this.handleResult.bind(this);
-        return 'mapCallback';
-    };
-
-
     /**
      * 将地图脚本插入到页面中
      *
      */
     BaiduMap.prototype.append = function () {
-        if (!this.config.ak) {
-            console.error('请配置服务密钥（ak）');
-            return;
-        }
-        var ele = document.createElement('script');
-        ele.src = this.mapUrl;
-        document.body.appendChild(ele);
-    };
-
-
-    /**
-     * JSON 解析，如果出错则在浏览器中进行提示
-     *
-     * @param {Object} json 地图参数回调还名称
-     * @return {Object|boolean}  解析成功返回 JSON 数据，否则返回 false
-     */
-    customElement.prototype.jsonParse = function (json) {
-        try {
-            return JSON.parse(json);
-        }
-        catch (e) {
-            console.error(e);
-            return false;
+        if (!baiduMapApiLoaded) {
+            if (!this.config.ak) {
+                console.error('请配置服务密钥（ak）');
+                return;
+            }
+            var ele = document.createElement('script');
+            ele.src = this.mapUrl;
+            document.body.appendChild(ele);
+            baiduMapApiLoaded = true;
         }
     };
 
-
     /**
-     * 处理请求返回后的结果，之后扩展逻辑均在该方法中实现
+     * 根据两点的经纬度计算出距离
      *
      */
-    BaiduMap.prototype.handleResult = function () {
+    BaiduMap.prototype.distance = function () {
         var selF = this;
 
         /* global BMap */
-        this.map = new BMap.Map(this.ele);
+        this.map = new window.BMap.Map(this.ele);
 
         var datas = {
             cp: {
@@ -172,13 +143,53 @@ define(function (require) {
 
 
     /**
+     * 根据目标位置的经纬度绘制地图
+     *
+     */
+    BaiduMap.prototype.drawMap = function () {
+        var selF = this;
+
+        /* global BMap */
+        selF.map = new window.BMap.Map(selF.ele);
+
+        selF.getTargetPosition().then(function (target) {
+            if (target && target.point)
+            {
+                var point = new window.BMap.Point(target.point.lng, target.point.lat);
+                selF.map.centerAndZoom(point, selF.config.zoom || 18);
+                selF.map.disableDragging();
+                var marker = new window.BMap.Marker(point);
+                selF.map.addOverlay(marker);
+            }
+        }, function () {
+            selF.ele.style.display = 'none';
+        });
+    };
+
+    /**
+     * 跳转到目标链接
+     *
+     */
+    BaiduMap.prototype.skipLink = function () {
+        var selF = this;
+        if (selF.config.link) {
+            selF.ele.addEventListener('click', function (evt) {
+                evt.stopPropagation();
+                evt.preventDefault();
+                window.location.href = selF.config.link;
+            });
+        }
+    };
+
+
+    /**
      * 获取当前位置经纬度
      * @return {Object}  成功返回当前位置信息
      */
     BaiduMap.prototype.getCurPosition = function () {
 
         /* global BMap */
-        var geolocation = new BMap.Geolocation();
+        var geolocation = new window.BMap.Geolocation();
 
         return new Promise(function (resolve, reject) {
             geolocation.getCurrentPosition(function (r) {
@@ -201,7 +212,7 @@ define(function (require) {
         var selF = this;
 
         /* global BMap */
-        var localSearch = new BMap.LocalSearch(this.map);
+        var localSearch = new window.BMap.LocalSearch(this.map);
 
         return new Promise(function (resolve, reject) {
             localSearch.setSearchCompleteCallback(function (res) {
@@ -224,11 +235,28 @@ define(function (require) {
         });
     };
 
-
-    customElement.prototype.firstInviewCallback = function () {
+    // build说明: 商家定位(需要请求百度地图API)，有多处用到，故用build
+    customElement.prototype.build = function () {
         var ele = this.element.querySelector(TYPE);
-        var cfg = this.jsonParse(ele.textContent);
-        cfg && new BaiduMap(this.element, cfg).show();
+        var cfg = jsonParse(ele.textContent);
+        if (cfg) {
+            var bMp = new BaiduMap(this.element, cfg);
+            bMp.init();
+            bMp.append();
+            window.addEventListener('load', function () {
+                switch (cfg.type) {
+                    case 'map':
+                        bMp.drawMap();
+                        bMp.skipLink();
+                        break;
+                    case 'distance':
+                        bMp.distance();
+                        break;
+                    default:
+                        bMp.distance();
+                }
+            });
+        }
     };
 
     return customElement;
