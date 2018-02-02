521e7defe80acb862c073855890f68a95616a525
mip-platform <mip-platform@baidu.com>
2018-02-02 09:58:30 +0800

    update extensions

--- mip-jia-log/mip-jia-log.js
+++ mip-jia-log/mip-jia-log.js
@@ -5,7 +5,9 @@
 
 define(function (require) {
     var customElement = require('customElement').create();
-    customElement.prototype.firstInviewCallback = function () {
+
+    // 页面进来就要加载百度统计,所以用build
+    customElement.prototype.build = function () {
         var element = this.element;
         // 封装加载js
         var loadScript = function (url, callback) {
@@ -30,7 +32,7 @@ define(function (require) {
         // 加载百度统计
         loadScript('https://rqs.baidu.com/static/js/loader.js?t=' + (new Date().getTime()), function () {
             /* global bdLog */
-            bdLog.init(element.getAttribute('pageType'), element.getAttribute('extInfo'));
+            bdLog.init(element.getAttribute('pageType'), element.getAttribute('extInfo'), 'qijia');
         });
     };
     return customElement;
