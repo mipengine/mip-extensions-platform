ffe4bd6403357c4e869bd385780ef8543d52dab1
mip-platform <mip-platform@baidu.com>
2018-03-07 11:21:09 +0800

    update extensions

---
+++ mip-jqtab-change/README.md
@@ -0,0 +1,40 @@
+# mip-jqtab-change
+
+mip-jqtab-change 使用jquery的tab切换功能
+
+标题|内容
+----|----
+类型|通用
+支持布局|responsive,fixed-height,fill,container,fixed
+所需脚本|https://c.mipcdn.com/static/v1/mip-jqtab-change/mip-jqtab-change.js
+
+## 示例
+
+### 基本用法
+```html
+<mip-jqtab-change lname="" target="">
+    
+</mip-jqtab-change>
+```
+
+## 属性
+
+### target
+
+说明：指向其控制显示隐藏元素的父元素
+必选项：否
+类型：{类型}
+取值范围：{取值范围}
+单位：{单位}
+默认值：无
+
+## 注意事项
+
+### lName
+
+说明：记录组件子元素类名
+必选项：否
+类型：{类型}
+取值范围：{取值范围}
+单位：{单位}
+默认值：无
---
+++ mip-jqtab-change/mip-jqtab-change.js
@@ -0,0 +1,29 @@
+/**
+ * @file mip-jqtab-change 组件
+ * @author
+ */
+
+define(function (require) {
+    'use strict';
+
+    var customElement = require('customElement').create();
+
+    /**
+     * 第一次进入可视区回调，只会执行一次
+     */
+    var $ = require('jquery');
+    customElement.prototype.firstInviewCallback = function () {
+        // TODO
+        var ele = this.element;
+        var str1 = ele.getAttribute('lName');
+        var id = ele.getAttribute('target');
+        var $p = $('#' + id);
+        $(ele).on('click', str1, function () {
+            $(this).addClass('active').siblings().removeClass('active');
+            var num = $(this).data('and');
+            $p.children('[data-and=' + num + ']').addClass('active').siblings().removeClass('active');
+        });
+    };
+
+    return customElement;
+});
