cbb4c161b19ef4d07d3a2b535fe4ee79d1adca9c
hejieye <ishejieye@126.com>
2018-05-30 12:39:45 +0800

    更新规范, 以及去掉无用方法 (#93)
    * Update mip-iask-ext.js
    
    * Update package.json
    
    * Update mip-iask-ext.js

--- mip-iask-ext/mip-iask-ext.js
+++ mip-iask-ext/mip-iask-ext.js
@@ -1,8 +1,8 @@
 /**
 * @file 脚本支持
 * @author  hejieye
-* @time  20170106
-* @version 1.3.2
+* @time  2018-05-22
+* @version 1.3.3
 */
 define(function (require) {
     var $ = require('zepto');
@@ -11,13 +11,13 @@ define(function (require) {
     var effects = {
         // 标签切换
         switchBlock: function () {
+            // 因为多个功能都集成在一个组件内，所以需要用到全局的选择器
             $('.similar-nav').on('click', 'li',
             function () {
                 event.preventDefault();
                 try {
                     $(this).siblings().removeClass('current');
                     $(this).addClass('current');
-
                     var index = $(this).index();
                     var nodes = $('.relative_kownlege').find('.tabs-con');
                     $(nodes).hide();
@@ -28,10 +28,11 @@ define(function (require) {
         },
         // 换一换
         changeMore: function () {
+            // 因为多个功能都集成在一个组件内，所以需要用到全局的选择器
             $('.link-change').on('click', function (event) {
                 event.preventDefault();
                 try {
-                    var pagesize = 5;
+                    var pagesize = parseInt($(this).attr('showSize'));
                     var childNodes = $(this).parent().next().children();
                     var pagecount = $(this).attr('pagecount');
                     if (!pagecount) {
@@ -49,21 +50,23 @@ define(function (require) {
             });
         },
         // 相关知识换一换
-        kownlegMore: function () {
-            $('.kownleg-change').on('click', function (event) {
+        kownlegMore: function() {
+            // 因为多个功能都集成在一个组件内，所以需要用到全局的选择器
+        	$('.kownleg-change').on('click', function (event) {
                 event.preventDefault();
-                $('div.similar').find('div.show').removeClass('show').addClass('hide').appendTo($('div.similar'));
-                var i = 1;
-                $('div.similar').find('div.hide').each(function () {
-                    if (i === 1) {
-                        $(this).removeClass('hide').addClass('show');
-                    }
-                    i ++;
-                });
-            });
+        		$("div.similar").find("div.show").removeClass("show").addClass("hide").appendTo($("div.similar"));
+        		var i = 1;
+        		$('div.similar').find('div.hide').each(function (){
+        			if(i == 1) {
+        				$(this).removeClass('hide').addClass('show');
+        			}
+        			i ++;
+        		});
+        	});
         },
-         // 展开 or 收起
+        // 展开 or 收起
         openOrStop: function () {
+            // 因为多个功能都集成在一个组件内，所以需要用到全局的选择器
             $('.os-click').on('click',
             function (event) {
                 event.preventDefault();
@@ -81,126 +84,38 @@ define(function (require) {
                 catch (e) {}
             });
         },
-        // 选择举报项
-        reportChange: function () {
-            $('.reportList li').on('click',
-            function () {
-                var fake = $(this).find('span').attr('class');
-                if (fake === 'fakeChecked') {
-                    $(this).find('span').removeClass();
-                    $(this).find('span').addClass('fakeCheck');
-                }
-                else {
-                    $(this).find('span').removeClass();
-                    $(this).find('span').addClass('fakeChecked');
-                }
-            });
-        },
-        clearReport: function () {
-            $('.report-body').hide();
-            $('.report_id').text('');
-            $('.report_type').text('');
-            $('.report_typeId').text('');
-            $('.reportList li').each(function () {
-                $(this).find('span').removeClass();
-                $(this).find('span').addClass('fakeCheck');
-            });
-        },
-        // 取消举报
-        cannelReport: function () {
-            $('.cannelReport').on('click',
-            function () {
-                effects.clearReport();
-            });
-        },
-        // 举报
-        okReport: function () {
-            $('.okReport').click(function () {
-                var reportList = '';
-                $('.reportList li').each(function () {
-                    var fake = $(this).find('span').attr('class');
-                    if (fake === 'fakeChecked') {
-                        reportList += $(this).text().trim() + '-';
-                    }
-                });
-                if (reportList === '') {
-                    alert('请选择举报原因！');
-                }
-                else {
-                    var checkLoginUrl = 'https://mipp.iask.cn/checkLogin?mip=' + Math.random();
-                    $.get(checkLoginUrl,
-                    function (e) {
-                        if (e == null || e === 'null') {
-                            // 跳转到登录页面
-                            var thisHref = window.location.href;
-                            window.location.href = 'https://mipp.iask.cn/login?source=' + thisHref;
-                        }
-                        else {
-                            var questionId = $('.report_id').text();
-                            var type = $('.report_type').text();
-                            var typeId = $('.report_typeId').text();
-                            $.post('http://m.iask.sina.com.cn/question/reportnew', {
-                                'reportList': reportList,
-                                'questionId': questionId,
-                                'type': type,
-                                'typeId': typeId
-                            },
-                            function (data) {
-                                var res = $.parseJSON(data);
-                                alert(res.desc);
-                            });
-                            effects.clearReport();
-                        }
-                    });
-                }
-            });
-        },
-         // 问题搜索
+        // 问题搜索
         btnSearch: function () {
+            // 因为多个功能都集成在一个组件内，所以需要用到全局的选择器
             $('.btn-search').click(function () {
                 var content = $('.search-input').val();
                 if (content.trim().length < 2) {
                     alert('关键字必须大于等于2个字!');
                     return;
                 }
-                window.location.href = 'http://m.iask.sina.com.cn/search/1.html?content=' + content;
+                effects.openUrl('https://mipp.iask.cn/search/1.html?content=' + content);
             });
         },
         // 提问
         btnSend: function () {
             try {
+                // 因为多个功能都集成在一个组件内，所以需要用到全局的选择器
                 $('.btn-send').click(function () {
                     event.preventDefault();
                     var content = $('.search-input').val();
-                    console.log(content);
-                    window.location.href = 'http://m.iask.sina.com.cn/ask?content=' + content;
+                    effects.openUrl('https://mipp.iask.cn/ask?content=' + content);
                 });
             }
             catch (e) {}
         },
         // 验证登录信息
         checkLogin: function () {
-            var btnUser = $('.btn-user');
-            var thisHref = window.location.href;
-            var checkLoginUrl = 'https://mipp.iask.cn/checkLogin?mip=' + Math.random();
-            $.get(checkLoginUrl,
-            function (e) {
-                if (e === null || e === 'null') {
-                    $('.icon-ency-login').attr('href', 'https://mipp.iask.cn/login?source=' + thisHref);
-                    btnUser.click(function (event) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                        $('.login-out').show();
-                    });
-                }
-                else {
-                    btnUser.click(function (event) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                        $('.login-in').show();
-                    });
-                }
-            });
+            // 因为多个功能都集成在一个组件内，所以需要用到全局的选择器
+            /*该参数是作为组件外部参数,所以需要用到全局选择器*/
+            var $that = document.querySelector('.paramDiv');
+    	    var cid = $that.getAttribute("cid");
+            var checkLoginUrl = 'https://mipp.iask.cn/checkLogin?mip=' + Math.random() + '&cid=' + cid;;
+            $.get(checkLoginUrl);
         },
         userInfoHide: function () {
             $(document).click(function (event) {
@@ -210,99 +125,54 @@ define(function (require) {
                 event.stopPropagation();
             });
         },
-        sendPost: function (url, positionId, advertId, status, contentId, connId) {
-            url = url + '?positionId=' + positionId + '&advertId='
-            + advertId + '&contentId=' + contentId + '&connId=' + connId;
-            if (status !== null) {
-                url += '&status=1';
-            }
-            $('body').append('<mip-pix src=\'' + url + '\' ></mip-pix>');
+        // 折叠
+        accordion : function () {
+            // 因为多个功能都集成在一个组件内，所以需要用到全局的选择器
+        	$('.iask-show-more').click(function () {
+        		$(this).parent().siblings('.iask-accordion').each(function () {
+        			$(this).show();
+        		});
+        		$(this).hide();
+        		$(this).siblings('.iask-show-less').show();
+        	});
+        	
+        	$('.iask-show-less').click(function () {
+        		$(this).parent().siblings('.iask-accordion').each(function () {
+        			$(this).hide();
+        		});
+        		$(this).hide();
+        		$(this).siblings('.iask-show-more').show();
+        	});
         },
-        // 数据上报
-        checkData: function () {
-            $('mip-ad,mip-embed').each(function () {
-                effects.sendPost('https://mipu.iask.cn/ddd/adAudit', '144', '241', null, '195', 'm_q_detail_attention_1');
-            });
-            setInterval(function () {
-                // 判断是否加载出来
-                $('mip-ad,mip-embed').each(function () {
-                    var loadFlag = $(this).attr('loadFlag');
-                    if ($(this).html().indexOf('iframe') > 0 && loadFlag === undefined) {
-                        $(this).attr('loadFlag', true);
-                        effects.sendPost('https://mipu.iask.cn/ddd/adStatus', '144', '241', 1, '195', 'm_q_detail_attention_1');
-                    }
-                });
-            }, 100);
-        },
-        accordion: function () {
-            $('.iask-show-more').click(function () {
-                $(this).parent().siblings('.iask-accordion').each(function () {
-                    $(this).show();
-                });
-                $(this).hide();
-                $(this).siblings('.iask-show-less').show();
-            });
-            $('.iask-show-less').click(function () {
-                $(this).parent().siblings('.iask-accordion').each(function () {
-                    $(this).hide();
-                });
-                $(this).hide();
-                $(this).siblings('.iask-show-more').show();
-            });
-        },
-        // 好万家导流
-        guideData: function () {
-            var urlf = 'https://mipp.iask.cn/t/mipdf?t=fous';
-            var urlr = 'https://mipp.iask.cn/t/mipdf?t=recom';
-            try {
-                $.ajax({
-                    type: 'GET',
-                    url: urlf,
-                    dataType: 'html',
-                    success: function (data) {
-                        if (!!data) {
-                            $('.load_today_focus').empty();
-                            $('.load_today_focus').append(data);
-                        }
-                    }
-                });
-                $.ajax({
-                    type: 'GET',
-                    url: urlr,
-                    dataType: 'html',
-                    success: function (data) {
-                        if (!!data) {
-                            $('.load_recom_red').empty();
-                            $('.load_recom_red').append(data);
-                        }
-                    }
-                });
-            }
-            catch (e) {
-                console.log(e);
-            }
+        openUrl : function (url) {
+        	var $that = document.querySelectorAll('.camnpr');
+        	if($that.length > 0) {
+        		for(var i=0; i<$that.length; i++) {
+        			var t = $that[i];
+        			t.parentNode.removeChild(t);
+        		}
+        	}
+             var a = document.createElement("a");  
+             a.setAttribute("href", url);  
+             a.setAttribute("class", "camnpr");  
+             ele.body.appendChild(a);  
+             a.click();
         },
         init: function () {
             this.switchBlock();
             this.changeMore();
             this.openOrStop();
-            this.reportChange();
-            this.cannelReport();
-            this.okReport();
             this.btnSearch();
             this.btnSend();
             this.checkLogin();
             this.userInfoHide();
-            // this.checkData();
             this.accordion();
-            this.guideData();
             this.kownlegMore();
         }
     };
 
     // build 方法，元素插入到文档时执行，仅会执行一次
     customElem.prototype.build = function () {
-        // this.element 可取到当前实例对应的 dom 元素
         effects.init();
     };
 
