ab2fac991a2e6a9f64f0ece23c2756b6025d45db
mip-platform <mip-platform@baidu.com>
2018-06-22 20:00:34 +0800

    update extensions

--- mip-catalog-reverse/mip-catalog-reverse.js
+++ mip-catalog-reverse/mip-catalog-reverse.js
@@ -11,7 +11,7 @@ define(function (require) {
      */
     customElement.prototype.firstInviewCallback = function () {
         // 给小说目录页（在组件外）逆序，需要操作组件外的dom结构，故必须要用全局选择
-        var catalog = document.querySelectorAll('.catalog-li li');
+        var catalog = document.querySelectorAll('.catalog-list li');
         var arra = [];
         arra = Array.prototype.slice.call(catalog);
         this.element.addEventListener('click', function () {
@@ -20,8 +20,7 @@ define(function (require) {
                 ss[i] = arra[i].innerHTML;
             };
 
-            for (var i = 0; i < ss.length; i++)
-            {
+            for (var i = 0; i < ss.length; i++) {
                 arra[i].innerHTML = ss[ss.length - 1 - i];
             };
 
