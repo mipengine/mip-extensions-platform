2505a69b13534c95c0f81fc48b5eab6af8c5c351
mip-platform <mip-platform@baidu.com>
2018-07-10 17:09:28 +0800

    update extensions

--- mip-huajun-xiazq/mip-huajun-xiazq.js
+++ mip-huajun-xiazq/mip-huajun-xiazq.js
@@ -2,14 +2,14 @@
 * fixed nav
 * @file fixed nav component
 * @author 873920193@qq.com
-* @version 1.1
+* @version 1.2
 * @copyright 2018 onlinedown.net, Inc. All Rights Reserved
 */
 define(function (require) {
     var $ = require('zepto');
     var customElem = require('customElement').create();
 
-    function init(isid, element) {
+    function init(isid, softtype, element) {
         var ba = document.referrer.indexOf('baidu') > -1;
         var bb = (isid === '1219170');
         var bb1 = (isid === '637958');
@@ -22,24 +22,27 @@ define(function (require) {
             $(element).find('.text1').show();
         }
         else {
-            $(element).find('em.icon').removeClass('checkhover');
-            if ($(element).find('#spdownload').attr('href')) {
-                $(element).find('#spdownload').show();
-                $(element).find('#download').hide();
+            if (softtype !== 'android') {
+                $(element).find('em.icon').removeClass('checkhover');
+                if ($(element).find('#spdownload').attr('href')) {
+                    $(element).find('#spdownload').show();
+                    $(element).find('#download').hide();
+                }
+                else {
+                    $(element).find('#spdownload').hide();
+                    $(element).find('#download').show();
+                }
+                $(element).find('.text1').hide();
+                $(element).find('.text2').show();
             }
-            else {
-                $(element).find('#spdownload').hide();
-                $(element).find('#download').show();
-            }
-            $(element).find('.text1').hide();
-            $(element).find('.text2').show();
         }
     }
     //
     customElem.prototype.firstInviewCallback = function () {
         var element = this.element;
         var hsId = $(element).attr('hsId');
-        init(hsId, element);
+        var softtype = $(element).attr('softtype');
+        init(hsId, softtype, element);
     };
     return customElem;
 });
