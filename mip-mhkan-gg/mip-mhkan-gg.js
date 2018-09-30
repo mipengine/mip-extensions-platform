78c138334d58bddf9649896ff451381be64671e8
mip-platform <mip-platform@baidu.com>
2018-09-30 14:06:37 +0800

    update extensions

---
+++ mip-mhkan-gg/mip-mhkan-gg.js
@@ -0,0 +1,24 @@
+/**
+ * @file 古风漫画网广告轮播组件
+ *
+ * @author shen@shenl.com
+ * @version 1.0.0
+ * @copyright 2018 shenl.com, Inc. All Rights Reserved
+ */
+
+define(function (require) {
+    var customElement = require('customElement').create();
+    var $ = require('zepto');
+
+    function render() {
+        var element = this.element;
+        var node = document.createElement('script');
+        node.type = 'text/javascript';
+        // 引入古风漫画网广告管理组件，实现广告素材轮播功能，服务提供商为：古风漫画网
+        node.src = 'https://www.mhkan.com/mip/a-mhkan.js';
+        node.async = 'async';
+        $(element).append(node);
+    }
+    customElement.prototype.build = render;
+    return customElement;
+});
