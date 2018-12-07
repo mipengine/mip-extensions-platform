217fb8c5f6c1b35f13f7434cabbbf050cb0c0259
mip-platform <mip-platform@baidu.com>
2018-12-07 11:00:13 +0800

    update extensions

--- mip-close/mip-close.js
+++ mip-close/mip-close.js
@@ -2,22 +2,6 @@
  * @file mip-close 组件
  * @author
  */
-(function (doc, win) {
-    var docEl = doc.documentElement;
-    var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
-    var recalc = function () {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) {
-                return;
-            }
-            docEl.style.fontSize = 40 * (clientWidth / 640) + 'px';
-        };
-    if (!doc.addEventListener) {
-        return;
-    }
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
-})(document, window);
 define(function (require) {
     'use strict';
 
@@ -27,6 +11,22 @@ define(function (require) {
      * 第一次进入可视区回调，只会执行一次
      */
     customElement.prototype.firstInviewCallback = function () {
+        (function (doc, win) {
+            var docEl = doc.documentElement;
+            var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+            var recalc = function () {
+                    var clientWidth = docEl.clientWidth;
+                    if (!clientWidth) {
+                        return;
+                    }
+                    docEl.style.fontSize = 40 * (clientWidth / 640) + 'px';
+                };
+            if (!doc.addEventListener) {
+                return;
+            }
+            win.addEventListener(resizeEvt, recalc, false);
+            doc.addEventListener('DOMContentLoaded', recalc, false);
+        })(document, window);
         var ele = this.element;
         ele.addEventListener('click', function () {
             var target = ele.parentElement;
