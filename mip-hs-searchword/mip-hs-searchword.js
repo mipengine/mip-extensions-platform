fef6f08335b88e7e411cd510a7d82c8a62a1dcc0
mip-platform <mip-platform@baidu.com>
2019-03-01 15:38:21 +0800

    update extensions

--- mip-hs-searchword/mip-hs-searchword.js
+++ mip-hs-searchword/mip-hs-searchword.js
@@ -13,14 +13,57 @@ define(function (require) {
 	 */
     customElement.prototype.firstInviewCallback = function () {
         var $el = $(this.element);
-        $el.find('.ser_btn').on('click', function () {
-            var serword = $('.serch_go').val();
+        var $form = $el.find('form');
+        var $input = $el.find('.serch_go');
+
+        function closearticle($modelbg, $model) {
+            $modelbg.fadeOut(300);
+            $model.fadeOut(300);
+        }
+
+        function openarticle($modelbg, $model, $text) {
+            $el.find('.text').text($text);
+            $modelbg.fadeIn(300);
+            $model.fadeIn(300);
+        }
+
+        function openAlart($showtext, callback) {
+            openarticle($el.find('.model_bg'), $el.find('.article_model'), $showtext);
+            if (callback) {
+                $el.find('.article_close').click(function () {
+                    closearticle($el.find('.model_bg'), $el.find('.article_model'));
+                    callback();
+                });
+            }
+        }
+
+        $el.find('.article_close').click(function () {
+            closearticle($el.find('.model_bg'), $el.find('.article_model'));
+        });
+
+        function serchsub() {
+            var serword = $el.find('.serch_go').val();
             if (serword !== '') {
                 window.top.location.href = '/search?keyword=' + serword;
             }
             else {
-                return alert('搜索内容不能为空！');
+                openarticle($el.find('.model_bg'), $el.find('.article_model'), '搜索内容不能为空');
+                //              return alert('搜索内容不能为空！');
             }
+        }
+
+        $el.find('.ser_btn').on('click', function () {
+            serchsub();
+        });
+        $form.on('keydown', function (e) {
+            var keycode = e.keyCode;
+            if (keycode === 13 || keycode === 9) {
+                e.preventDefault();
+                serchsub();
+                $input.blur();
+                return false;
+            }
+
         });
     };
 
