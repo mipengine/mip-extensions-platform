d71dd1f93e7428f6db87149c1968a25aeb7b37bd
mip-platform <mip-platform@baidu.com>
2019-04-12 16:41:53 +0800

    mip-platform auto create Pull Request

---
+++ mip-qqtn-addwxbtn/README.md
@@ -0,0 +1,41 @@
+# mip-qqtn-addwxbtn
+
+mip-qqtn-addwxbtn 获取配置文件地区以及信息进行判断，覆盖原始内容
+
+标题|内容
+----|----
+类型|通用
+所需脚本|https://c.mipcdn.com/static/v1/mip-qqtn-addwxbtn/mip-qqtn-addwxbtn.js
+
+## 示例
+
+### 基本用法
+```html
+<style mip-custom="">
+
+.g-cms-content h1 {width: 100%;height: auto;line-height: 34px;font-size: 22px;font-weight: bold;color: #000;text-align: center;display: block;overflow: hidden;padding: 10px 10px 5px 10px;}
+.f-hide { display: none }
+
+</style>
+
+
+
+<mip-qqtn-addwxbtn data-shield="qqtn">
+
+    <article class="g-cms-content">
+        <h1>王者荣耀2.0更新后闪退进不去 王者荣耀闪退后特效消失怎么办</h1>
+        <mip-fy-hits data-hitsurl="https://m.qqtn.com/ajax.asp?Action=4&amp;id=" data-hitsid="275404" class="mip-element mip-layout-container">
+        <em><time pubdate="pubdate">时间:2019/1/17 16:48:00</time>  作者:网友整理  人气:<span id="hits">1719</span></em>
+        </mip-fy-hits>
+    </article>
+    
+<div class="f-infopzwj f-hide" data-star="2"></div>
+</mip-qqtn-addwxbtn>
+
+
+
+
+
+
+
+</body></html>
---
+++ mip-qqtn-addwxbtn/mip-qqtn-addwxbtn.js
@@ -0,0 +1,43 @@
+﻿/**
+ * @file mip-qqtn-addwxbtn
+ * 获取配置文件地区以及信息进行判断，覆盖原始内容
+ * @author
+ */
+define(function (require) {
+    var $ = require('zepto');
+    var customElement = require('customElement').create();
+    var fetchJsonp = require('fetch-jsonp');
+    customElement.prototype.firstInviewCallback = function () {
+        var ele = this.element;
+        var fhieldur = ele.getAttribute('data-shield');
+        // 获取配置文件地区信息
+        fetchJsonp('https://ca.6071.com/shield/index/c/' + fhieldur, {
+            jsonpCallback: 'callback'
+        }).then(function (res) {
+            return res.json();
+        }).then(function (data) {
+            var starok = data['wxaddbtnOK'];
+            // 配置文件是否启用
+            if (starok) {
+                // 启用配置文件 执行添加按钮
+                var wxgzbtn = '<em class="newsnext foucs_dyh"><a href="javascript:;">关注+</a></em>';
+                $(ele).find('.g-cms-content em').append(wxgzbtn);
+                // 添加配置文件内容
+                $(ele).find('.g-cms-content').after(data.addwxbtn[0]);
+                $(ele).find('.foucs_dyh').click(function () {
+                // 执行点击事件
+                    $(ele).find('.wefocus_bg').show();
+                    $(ele).find('.wefocus_a').show();
+                    $(ele).find('.wefocus_a').css('margin-top', - $(ele).find('.wefocus_a').height() / 2 + 'px');
+                });
+                $(ele).find('.wefocus_bg,.wefocus_clo').click(function () {
+                    $(ele).find('.wefocus_a,.wefocus_bg').hide();
+                });
+            }
+        });
+    };
+    return customElement;
+});
+
+
+
