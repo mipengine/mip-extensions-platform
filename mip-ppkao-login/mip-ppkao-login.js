9814854e0d61b602a41ae6aaf3b029eb00a70e9f
mip-platform <mip-platform@baidu.com>
2019-05-08 11:20:19 +0800

    mip-platform auto create Pull Request

--- mip-ppkao-login/mip-ppkao-login.js
+++ mip-ppkao-login/mip-ppkao-login.js
@@ -18,7 +18,7 @@ define(function (require) {
         $(document).ready(function () {
             $.ajax({
                 type: 'get',
-                url: 'https://api.ppkao.com/user/IsLogin.ashx?action=UserIsLogin',
+                url: 'https://api.ppkao.com/user/IsLogin.ashx?action=UserIsLogin&Source=mip',
                 dataType: 'jsonp',
                 contentType: 'application/x-www-form-urlencoded;charset=utf-8',
                 jsonp: 'callback',
